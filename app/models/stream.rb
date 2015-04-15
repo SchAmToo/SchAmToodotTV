f1ef4dc9d43584067cab854c9881a61f1486b623
Schamtoo <thekevinpearson@gmail.com>
2015-04-15 00:03:43 -0400

    modified:   app/models/stream.rb modified:   app/views/streams/show.html.erb 	modified:   app/views/welcome/frontpage.html.erb 	modified:   log/development.log 	new file:   tmp/pids/server.pid
     Untracked files:
    	.bundle/

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -3,12 +3,18 @@ class Stream < ActiveRecord::Base
 
 	def self.checkOnline()
 		
-		mlgstreamapi = "http://streamapi.majorleaguegaming.com/service/streams/status/mlg302?callback=callback"
-		mlg = Nokogiri::HTML(mlgstreamapi)
-		if mlg.text.match('2') 
+		mlgstreamapi = open("http://streamapi.majorleaguegaming.com/service/streams/status/mlg302?callback=callback")
+
+		mlg = Nokogiri::HTML(mlgstreamapi)		
+
+		if mlg.text.match('"status": 1') 
 			return "http://tv.majorleaguegaming.com/player/embed/schamtoo"
 		else
 			return "https://www.youtube.com/embed/cEJhLiFhz1k"
 		end
 	end
 end
+
+# https://api.twitch.tv/kraken/streams/milktea <--- stream: null == offline
+#		twitch = open("https://api.twitch.tv/kraken/streams/shaboozey")
+#		regextwitch = pullviewers.text.match(/"viewers":(\d*?),/)[1]
