6174b122df767388473768e1892231c6e6f81988
Schamtoo <thekevinpearson@gmail.com>
2015-10-25 22:52:02 -0400

    Fixed caching for good

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -1,7 +1,7 @@
 module StreamsHelper
 	
 	def pause_or_play(stream)
-		if stream.is_stream_live
+		if Stream.live_streams.include?(stream)
 			return "<i class = 'glyphicon glyphicon-play' ></i>".html_safe
 		else
 			return "<i class = 'glyphicon glyphicon-pause' ></i>".html_safe
--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,29 +1,33 @@
 require 'open-uri'
 class Stream < ActiveRecord::Base
-	def set_stream_live?
-	 	if last_time_checked < (Time.now - 2*60)
-	 		Stream.find_streams_online
+
+ 	def self.cache_stale?
+ 		last_time_checked < (Time.now - 2*60)
+ 	end
+
+ 	def self.live_streams
+ 		if cache_stale?
+	 		@live_streams = Stream.find_online_streams
 	 		last_time_checked = Time.now
  		end
- 		if Stream.find_streams_online.include?(self)
- 			return true
-		else
-			return false
-		end
-	end
+ 		@live_streams
+ 	end
 
-	def self.find_streams_online
+	def self.find_online_streams
 		Stream.all.select do |stream|
-			stream.checkOnline?
+			stream.is_live?
 		end
 	end
 
+	def self.last_time_checked=(value)
+		@last_time_checked=value
+	end
 
-	def last_time_checked
+	def self.last_time_checked
 		@last_time_checked ||= ( Time.now - 2*60)
 	end 
 
-	def checkOnline?
+	def is_live?
 		nokogiri_scrape = Nokogiri::HTML(open(self.stream_api_url))
 		nokogiri_scrape.text.match(self.text_to_scrape)
 	end
