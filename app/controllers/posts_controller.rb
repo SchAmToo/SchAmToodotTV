69245a773a0739de0e90eb5369ffe29d58a1ce70
Schamtoo <thekevinpearson@gmail.com>
2015-11-18 21:17:50 -0500

    modified:   app/controllers/posts_controller.rb 	modified:   app/views/welcome/frontpage.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,14 @@ class PostsController < ApplicationController
 	end
 	def update
 		@post = Post.find(params[:id])
-		if @post.update(params[:post].permit(:title, :text, :remote_url))
+		if @post.update(params[:post].permit(:title, :text, :remote_url, :blurb))
 			redirect_to @post
 		else
 			render 'edit'
 		end
 	end
 	def create
-		@post = Post.new(params[:post].permit(:title, :text, :remote_url))
+		@post = Post.new(params[:post].permit(:title, :text, :remote_url, :blurb))
   		@post.user = current_user
 		if @post.save
 			redirect_to @post
